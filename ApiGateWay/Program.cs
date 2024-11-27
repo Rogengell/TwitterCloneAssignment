0e78cdded4f8d4872174e6969a78a9058d871e5f
Jan_H_Christensen <jan.christensen1981@gmail.com>
2024-11-27 15:43:34 +0100

    ✨ fluentd sidecar implemented

--- ApiGateWay/Program.cs
+++ ApiGateWay/Program.cs
@@ -9,14 +9,21 @@ using Vault.Model;
 using Vault.Client;
 using Newtonsoft.Json.Linq;
 using Serilog;
-using Serilog.Events;
+using Serilog.Sinks.Fluentd;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration.GetSection("Settings").Get<Settings>();
 
+var fluentdHost = Environment.GetEnvironmentVariable("FLUENTD_HOST") ?? "fluentd";
+var fluentdPort = int.Parse(Environment.GetEnvironmentVariable("FLUENTD_PORT") ?? "24224");
+
 Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()  
-    .WriteTo.Http("http://fluentd:24224")  
+    .MinimumLevel.Information()
+    .WriteTo.Fluentd(
+        host: fluentdHost,
+        port: fluentdPort,
+        tag: "apigateway"  // Optional tag
+    )
     .CreateLogger();
 
 builder.Host.UseSerilog();
@@ -33,14 +40,14 @@ builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddSingleton(
-    (ServiceProvider) => 
+    (ServiceProvider) =>
     {
         VaultConfiguration vaultConfig = new VaultConfiguration(config.VoultHostName);
         VaultClient client = new VaultClient(vaultConfig);
 
-        var authResponse = client.Auth.UserpassLogin("user",new UserpassLoginRequest("1234"));
+        var authResponse = client.Auth.UserpassLogin("user", new UserpassLoginRequest("1234"));
         client.SetToken(authResponse.ResponseAuth.ClientToken);
-        VaultResponse<KvV2ReadResponse> response = client.Secrets.KvV2Read("secret", "MicroServiceToken");
+        VaultResponse<KvV2ReadResponse> response = client.Secrets.KvV2Read("secret", "secret");
         JObject data = (JObject)response.Data.Data;
         SecretSettings secretSettings = data.ToObject<SecretSettings>();
 
